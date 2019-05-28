fac8086b553147f94e92c95d02db598ceb8c8725
Jovin Mwilanga <jovinbeda@hotmail.com>
2019-05-27 21:01:15 -0400

    use uuid for temporary folders

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as aws from 'aws-sdk';
 import fs from 'fs';
 import mkdirp from 'mkdirp';
 import path from 'path';
+import uuidV4 from 'uuid/v4';
 
 const S3 = new aws.S3();
 
@@ -12,67 +13,77 @@ export interface ILambdaProcessImagesEvent {
   // output key = path.join(output_directory, each processed file name)
   output_bucket: string;
   output_directory: string;
-  put_object_options: Partial<aws.S3.Types.PutObjectRequest>
-  versions: IVersion[]
+  put_object_options: Partial<aws.S3.Types.PutObjectRequest>;
+  versions: IVersion[];
 }
 
 export interface ILambdaProcessImagesSuccessPayload {
-  error_message?: string
+  error_message?: string;
   data?: {
     // e.g. 80: 'images/140619_5872_aspR_1.5_w4898_h3265_e80.jpg'
-    [key: string]: string
-  }
+    [key: string]: string;
+  };
 }
 
-export type TLambdaSuccessFunction = (payload: ILambdaProcessImagesSuccessPayload) => any
+export type TLambdaSuccessFunction = (
+  payload: ILambdaProcessImagesSuccessPayload
+) => any;
 
-export const handler = async (event: ILambdaProcessImagesEvent, context: {
-  succeed: TLambdaSuccessFunction
-}) => {
+export const handler = async (
+  event: ILambdaProcessImagesEvent,
+  context: {
+    succeed: TLambdaSuccessFunction;
+  }
+) => {
   try {
-    const folder_input = '/tmp/images/input';
-    const folder_output = '/tmp/images/output';
+    const folder_input = `/tmp/images/input/${uuidV4()}`;
+    const folder_output = `/tmp/images/output/${uuidV4()}`;
 
     console.log('Creating input and output folders.');
-    await Promise.all([
-      folder_input,
-      folder_output,
-    ].map(p => {
-      return new Promise((resolve, reject) => {
-        mkdirp(p, (err) => {
-          if (err) {
-            reject(err);
-          }
-          else {
-            resolve(true);
-          }
+    await Promise.all(
+      [folder_input, folder_output].map(p => {
+        return new Promise((resolve, reject) => {
+          mkdirp(p, err => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(true);
+            }
+          });
         });
-      });
-    }));
+      })
+    );
 
     console.log(`Getting bucket=${event.bucket}, key=${event.key} from s3.`);
     const file: Buffer = await new Promise<Buffer>((resolve, reject) => {
-      S3.getObject({
-        Bucket: event.bucket,
-        Key: event.key,
-      }, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data.Body as Buffer);
+      S3.getObject(
+        {
+          Bucket: event.bucket,
+          Key: event.key,
+        },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data.Body as Buffer);
+          }
         }
-      });
+      );
     });
 
     console.log(`Writing file to input folder.`);
     await new Promise((resolve, reject) => {
-      fs.writeFile(path.join(folder_input, path.basename(event.key)), file, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
+      fs.writeFile(
+        path.join(folder_input, path.basename(event.key)),
+        file,
+        error => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
         }
-      });
+      );
     });
 
     console.log('Processing files.');
@@ -85,7 +96,10 @@ export const handler = async (event: ILambdaProcessImagesEvent, context: {
     // processImages only returns the new file names. We want this result to contain
     // the full key of where the image lives in s3
     Object.keys(processing_results).map(version_height => {
-      processing_results[version_height] = path.join(event.output_directory, processing_results[version_height]);
+      processing_results[version_height] = path.join(
+        event.output_directory,
+        processing_results[version_height]
+      );
     });
 
     console.log('Uploading processed files back to S3.');
@@ -98,19 +112,24 @@ export const handler = async (event: ILambdaProcessImagesEvent, context: {
           file_names.map(file_name => {
             uploadPromises.push(
               new Promise((resolve, reject) => {
-                S3.putObject({
-                  ...event.put_object_options,
-                  Bucket: event.output_bucket,
-                  Key: path.join(event.output_directory, file_name),
-                  Body: fs.createReadStream(path.join(folder_output, file_name)),
-                }, (err) => {
-                  if (err) {
-                    reject(err);
-                  } else {
-                    resolve();
+                S3.putObject(
+                  {
+                    ...event.put_object_options,
+                    Bucket: event.output_bucket,
+                    Key: path.join(event.output_directory, file_name),
+                    Body: fs.createReadStream(
+                      path.join(folder_output, file_name)
+                    ),
+                  },
+                  err => {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      resolve();
+                    }
                   }
-                });
-              }),
+                );
+              })
             );
           });
           resolve();
@@ -123,8 +142,7 @@ export const handler = async (event: ILambdaProcessImagesEvent, context: {
     context.succeed({
       data: processing_results,
     });
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err);
     context.succeed({
       error_message: err.message,
